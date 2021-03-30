e0d9f3dbdcc879def368d927a34fe4c5a6be3b58
randymarsh77 <mattpsbs@gmail.com>
2021-03-29 21:37:44 -0700

    Fix module.

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,1 @@
-#!/usr/bin/env node
-
-import { runCLI } from '@simple-cli/base';
-import { commands } from './commands';
-
-runCLI({
-	name: 'tsa',
-	version: '1.0.0',
-	summary: 'Hey-oh',
-	commands,
-});
+export * from './types';
